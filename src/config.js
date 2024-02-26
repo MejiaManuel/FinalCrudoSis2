7293b8d37a0a45a8a787123af289175de8923ecb
MejiaManuel <manuelmejia.dev@gmail.com>
2024-02-25 22:39:07 -0300

    conectado a mongo y ver

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 import {config} from 'dotenv';
 
 config()
-
+console.log(process.env)
 export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/test';
+
 
 export const PORT = process.env.PORT || 3000;
